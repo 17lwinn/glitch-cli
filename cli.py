60ffe59fc0e880a37f3d705fd3f81a06919443c3
Glitch (hello-webpage) <none>
2020-05-12 16:25:46 +0000

    🐌🐙 Checkpoint ./cli.py:5152800/845

--- cli.py
+++ cli.py
@@ -1,4 +1,7 @@
 import cmd
+import os
+from time import *
+
 
 class GlitchCli(cmd.Cmd):
   
@@ -7,15 +10,29 @@ class GlitchCli(cmd.Cmd):
     def do_greet(self, line):
         print ("hello user, hope you have a good day using our service")
     
+    def do_node(self, line):
+        print("are you sure you want to make your project a node app? Y or N")
+        node = input("choice: ")
+        
+        if node == "Y":
+          print("I REALLY hope you know what your doing")
+          print("prepare for hyperspace... NODIFY!")
+          sleep(2)
+          os.system("curl https://nodify.glitch.me|bash")
+          
+    
     def do_exit(self, line):
         return True
 # -----------------------------------------------------------------------
 #                              help
 # -----------------------------------------------------------------------
     def help_greet(self):
-         print ([ "greet",
-                  "Greet the current user",
-                        )
+         print ( "greet: ", "Greet the current user")
+      
+    def help_exit(self):
+         print ( "exit: ", "exit the CLI")   
+    def help_exit(self):
+         print ( "exit: ", "exit the CLI")
         
         
         
