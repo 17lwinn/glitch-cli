91467868987b06c5629c1290de93795b51a86560
Glitch (hello-webpage) <none>
2020-05-13 09:04:43 +0000

    🐈🍶 Checkpoint ./cli.py:5152800/791

--- cli.py
+++ cli.py
@@ -33,6 +33,22 @@ class GlitchCli(cmd.Cmd):
       zip_file.close()
       sleep(2)
       
+    def do_push(self, line):
+      print("where would you like to push your code to? 1 or 2")
+      print("1. github")
+      print("2. github gists")
+      git = input("push my code to: ")
+      
+      if git == "1":
+        github = input("repo URL: ")
+        print("pushing... you will be queried for your credentials by git")
+        os.system("git remote add origin " + github)
+        os.system("git push -u origin master")
+        sleep(2)
+        print("good news! your changes have been commited!")
+        print("if you want to push to a gist, run the ")
+      
+      
     def do_exit(self, line):
         return True
 # -----------------------------------------------------------------------
